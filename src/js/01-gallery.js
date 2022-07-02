8b33ff4525449650b4af3fde2d445c26af5760f7
Maksym Pokulytyi <mlightpro@gmail.com>
2022-07-02 22:20:05 +0300

    add 8.2 +lodash(throttle)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,24 +1,22 @@
-// Add imports above this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 
 const refs = {
-  gallery: document.querySelector(".gallery"),
+  gallery: document.querySelector('.gallery'),
 };
 const markup = galleryItems
   .map(({ preview, original, description }) => {
     return `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" /></a>`;
   })
-  .join("");
+  .join('');
 
-refs.gallery.insertAdjacentHTML("beforeend", markup);
+refs.gallery.insertAdjacentHTML('beforeend', markup);
 
-const lightbox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
   captionDelay: 250,
 });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,21 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+const player = new Player(iframe);
+const STORAGE_TIME = 'videoplayer-current-time';
+// метод .on відстежує подію timeupdate ( тротлом уповільнємо безіменну функцію, яка приймає значення
+// події timeupdate у змінну moveTime)
+// в localStorage записуємо властивість seconds, яка знаходиться у об'єкта moveTime.
+player.on(
+  'timeupdate',
+  throttle(function (moveTime) {
+    localStorage.setItem(STORAGE_TIME, moveTime.seconds);
+  }, 1000)
+);
+//перевірка чи пустий сторедж, якщо не пустий то плеєру вказуємо початковий час
+if (localStorage.getItem(STORAGE_TIME) !== '') {
+  const savedData = localStorage.getItem(STORAGE_TIME);
+  player.setCurrentTime(savedData);
+}
