37d1003cb75c1f80f851556df9db4e546d5757b9
Maksym Pokulytyi <mlightpro@gmail.com>
2022-07-04 22:43:08 +0300

    add the check to localStorage 8.3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,7 @@
 import throttle from 'lodash.throttle';
 const STORAGE__KEY = 'feedback-form-state';
-const formData = {};
+let formData = {};
+let parsedData = {};
 const refs = {
   form: document.querySelector('form'),
   input: document.querySelector('input'),
@@ -13,15 +14,14 @@ refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onInputText, 500));
 //виклик функції яка бере інформацію із localStorage та записує її в поля
 onGetDataToInputs();
+
 // заборона дій браузера по замовчуванням
 // при відправці форми очищаємо форму
 // та очищаємо localStorage
 function onFormSubmit(event) {
   event.preventDefault();
-
   event.target.reset();
   localStorage.removeItem(STORAGE__KEY);
-  console.log(formData);
 }
 
 // додавши слухача на всю форму, якщо об'єкт formData пустий записуємо в пустий об'єкт
@@ -29,15 +29,20 @@ function onFormSubmit(event) {
 // перетворюємо об'єкт на строку
 // записуємо в localStorage цю строку
 function onInputText(e) {
-  formData[e.target.name] = e.target.value;
+  // console.log(localStorage);
 
+  // console.log(localStorage.getItem(STORAGE__KEY));
+
+  formData = localStorage.getItem(STORAGE__KEY) ? parsedData : {};
+  formData[e.target.name] = e.target.value;
   const savedData = JSON.stringify(formData);
   localStorage.setItem(STORAGE__KEY, savedData);
+  console.log(formData);
 }
 
 // отримуємо STORAGE__KEY (значення) із сховища, якщо там щось було оновлюємо DOM
 function onGetDataToInputs() {
-  const parsedData = JSON.parse(localStorage.getItem(STORAGE__KEY));
+  parsedData = JSON.parse(localStorage.getItem(STORAGE__KEY));
   if (parsedData) {
     refs.input.value = parsedData.email ? parsedData.email : '';
     refs.textarea.value = parsedData.message ? parsedData.message : '';
